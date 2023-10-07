07cd86293c2ba2e837f841a6de2a5b37cdf6d865
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2023-10-07 19:44:11 +0200

    Update milonerzy.py

--- milonerzy.py
+++ milonerzy.py
@@ -4,7 +4,6 @@ from tkinter import *
 import mysql.connector
 
 
-
 import MySQLdb
 
 milionerzy = MySQLdb.connect(host="localhost",    # your host, usually localhost
@@ -49,32 +48,23 @@ play_btn.place(x=700, y=700)
 def informacje():
     extra_window = tkk.Toplevel()
     extra_window.geometry('1920x1080')
-
-    bg1 = tkk.PhotoImage(file = "milionerzy1.png")
-
-    play_img1 = tkk.Label(extra_window, image=bg1)
-    play_img1.place(x=0, y=0)
-
-    
-
-
-
-    zas = Label(extra_window, text=" ZASADY", width = 8, height= 2, font=('Times New Roman', 20, 'bold')).place(x= 310,y= 300)
+    bg2 = tkk.PhotoImage(file = "milionerzy1.png")
+    bg3 = tkk.Label(extra_window, image=bg2)
+    bg3.place(x=0, y=0)
+    zas = Label(extra_window, text=" ZASADY", bg="#2c397f", highlightthickness=10, width = 8, height= 2, font=('Times New Roman', 20, 'bold')).place(x= 310,y= 120)
     zasady_gry = Label(extra_window, text = "\nGra milionerzy polega na tym, aby \n odpowiedzieć poprawnie na 12 pytań, za\n każde pytanie jest dane suma pieniędzy. \n W grze są 2 progi gwarantowane \n 1000 pln i 40000 pln.\nW każdej rundzie uczestnik \n dostanie pytanie oraz cztery \n odpowiedzi, spośród których musi \n wybrać jedną właściwą. Każdy \n zawodnik ma prawo do wykorzystania \n trzech kół ratunkowych \n w dowolnym czasie gry.\n",
-                    width=35,height=15, font=('Times New Roman', 20, 'bold')).place(x= 75,y= 400)
-    kolorat1 = Label(extra_window, text ="koło ratunkowe, które eliminuje dwie błędne odpowiedzi", width=50,height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 300)
-    kolorat2 = Label(extra_window, text ="koło ratunkowe, które elimunuje jedną złą odpowiedź", width=50,height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 575)
-    kolorat3 = Label(extra_window, text ="koło ratunkowe, które wskazuje poprawną odpowiedź", width=50, height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 865)
-
-    
+                        bg="#2c397f", highlightthickness=10, width=35,height=15, font=('Times New Roman', 20, 'bold')).place(x= 100,y= 270)
+    kolorat1 = Label(extra_window, text ="koło ratunkowe, które eliminuje dwie błędne odpowiedzi", bg="#2c397f", highlightthickness=10,  width=50,height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 150)
+    kolorat2 = Label(extra_window, text ="koło ratunkowe, które elimunuje jedną złą odpowiedź", bg="#2c397f", highlightthickness=10, width=50,height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 500)
+    kolorat3 = Label(extra_window, text ="koło ratunkowe, które wskazuje poprawną odpowiedź", bg="#2c397f", highlightthickness=10, width=50, height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 850)
 
     image1= tkk.PhotoImage(file = "guzik-50na50.png")
     kl1 = tkk.Label(extra_window, image=image1)
-    kl1.place(x=700, y=250)
+    kl1.place(x=700, y=100)
 
     image2 = tkk.PhotoImage(file = "guzik-eliminacja1zlejodp.png")
     kl2 = tkk.Label(extra_window,image=image2)
-    kl2.place(x=700, y=525)
+    kl2.place(x=700, y=450)
 
     image3 = tkk.PhotoImage(file = "guzik-teldoprzyj.png")
     kl3 = tkk.Label(extra_window, image=image3)
@@ -88,7 +78,7 @@ info = tkk.PhotoImage(file="guzik-informacje.png")
 info_btn = tkk.Button(root, image=info, command=informacje)
 info_btn.place(x=1685, y=770)
 
-
                        
 root.mainloop()
+
 
