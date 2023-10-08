74043e98a6c03609b56e12e57f13da96ed40d6fd
ItsIzabela <146938246+ItsIzabela@users.noreply.github.com>
2023-10-08 13:42:27 +0200

    Add files via upload

--- milonerzy.py
+++ milonerzy.py
@@ -11,23 +11,21 @@ milionerzy = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      passwd="",  # your password
                      db="milionerzy")        # name of the data base
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
+
 cur = milionerzy.cursor()
+zestawy = [
+cur.execute("SELECT Pytania FROM pyt WHERE id BETWEEN 1 AND 12"), 
+cur.execute("SELECT Pytania FROM pyt WHERE id BETWEEN 13 AND 24"),
+cur.execute("SELECT Pytania FROM pyt WHERE id BETWEEN 25 AND 36"),
+cur.execute("SELECT Pytania FROM pyt WHERE id BETWEEN 37 AND 48")]
 
-# Use all the SQL you like
-cur.execute("SELECT * FROM pyt ")
-cur.execute("SELECT * FROM odp ")
-
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print ("Poprawna odp: ", row[5])
-
-
+zes_wyb = random.choices(zestawy)
+print(milionerzy)
 
 root = tkk.Tk()
 root.geometry('1920x1080')
 root.configure(bg="black")
+
 
 # wejscie do gry
 
@@ -35,13 +33,6 @@ bg = tkk.PhotoImage(file = "milionerzy1.png")
 
 play_img = tkk.Label(image=bg)
 play_img.place(x=0, y=0)
-
- # przycisk Zagraj 
-
-play = tkk.PhotoImage(file="guzik-zagraj.png")
-
-play_btn = tkk.Button(root, image=play)
-play_btn.place(x=700, y=700)
 
 # przycisk Info
 
@@ -58,16 +49,16 @@ def informacje():
     kolorat2 = Label(extra_window, text ="koło ratunkowe, które elimunuje jedną złą odpowiedź", bg="#2c397f", highlightthickness=10, width=50,height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 570)
     kolorat3 = Label(extra_window, text ="koło ratunkowe, które wskazuje poprawną odpowiedź", bg="#2c397f", highlightthickness=10, width=50, height=3, font=('Times New Roman', 20, 'bold')).place(x= 1000,y= 850)
 
-    image1= tkk.PhotoImage(file = "guzik-50na502.png")
-    kl1 = tkk.Label(extra_window, image=image1, highlightthickness=0)
+    image1= tkk.PhotoImage(file = "guzik-50na50.png")
+    kl1 = tkk.Label(extra_window, image=image1)
     kl1.place(x=700, y=250)
 
-    image2 = tkk.PhotoImage(file = "guzik-eliminacja1zlejodp2.png")
-    kl2 = tkk.Label(extra_window,image=image2, highlightthickness=0)
+    image2 = tkk.PhotoImage(file = "guzik-eliminacja1zlejodp.png")
+    kl2 = tkk.Label(extra_window,image=image2)
     kl2.place(x=700, y=525)
 
-    image3 = tkk.PhotoImage(file = "guzik-teldoprzyj2.png")
-    kl3 = tkk.Label(extra_window, image=image3, highlightthickness=0)
+    image3 = tkk.PhotoImage(file = "guzik-teldoprzyj.png")
+    kl3 = tkk.Label(extra_window, image=image3)
     kl3.place(x=700, y=800)
 
     extra_window.mainloop()
@@ -77,6 +68,26 @@ info = tkk.PhotoImage(file="guzik-informacje.png")
 
 info_btn = tkk.Button(root, image=info, command=informacje)
 info_btn.place(x=1685, y=770)
+
+# przycisk Zagraj 
+
+def gra():
+    play_window = tkk.Toplevel()
+    play_window.geometry('1920x1080')
+    bg4 = tkk.PhotoImage(file = "milionerzy2.png")
+    bg5 = tkk.Label(play_window, image=bg4)
+    bg5.place(x=0, y=0)
+    tab1 = tkk.PhotoImage(file = "tablica.png")
+    tab_1 = tkk.Label(play_window, image=tab1)
+    tab_1.place(x=1300,y=-40)
+
+    play_window.mainloop()
+
+
+play = tkk.PhotoImage(file="guzik-zagraj.png")
+
+play_btn = tkk.Button(root, image=play, command=gra)
+play_btn.place(x=700, y=700)
 
                        
 root.mainloop()
